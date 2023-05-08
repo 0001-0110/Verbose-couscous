a01d3f677f48bc4e519513e59d754b4198320a29
Rémi TROTEL <remi.trotel@epita.fr>
2023-05-08 14:08:42 +0200

    Created projects

---
+++ Verbose-couscous.DAL/src/main/java/hr/algebra/verbose_couscous/dal/VerboseCouscousDAL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hr.algebra.verbose_couscous.dal;
+
+/**
+ *
+ * @author remi
+ */
+public class VerboseCouscousDAL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Verbose-couscous.HMI/src/main/java/hr/algebra/verbose_couscous/hmi/VerboseCouscousHMI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hr.algebra.verbose_couscous.hmi;
+
+/**
+ *
+ * @author remi
+ */
+public class VerboseCouscousHMI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
