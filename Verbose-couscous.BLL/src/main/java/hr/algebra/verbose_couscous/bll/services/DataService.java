4732c6384051eecb1a4e4bd6b8a6cfffaa2b96e8
Rémi TROTEL <remi.trotel@epita.fr>
2023-06-22 14:07:38 +0200

    data service

--- Verbose-couscous.BLL/src/main/java/hr/algebra/verbose_couscous/bll/services/DataService.java
+++ Verbose-couscous.BLL/src/main/java/hr/algebra/verbose_couscous/bll/services/DataService.java
@@ -4,14 +4,12 @@
  */
 package hr.algebra.verbose_couscous.bll.services;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import hr.algebra.verbose_couscous.dal.models.Model;
 import hr.algebra.verbose_couscous.dal.repositories.IRepository;
 import hr.algebra.verbose_couscous.dal.repositories.IRepositoryCollection;
-import hr.algebra.verbose_couscous.dal.services.DatabaseService;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 /**
  *
@@ -26,45 +24,26 @@ public class DataService {
     }
 
     private <T extends Model> IRepository getRepository(Class<T> type) {
-        return repositoryCollection.getRepository(type).get();
+        return repositoryCollection.getRepository(type);
     }
 
-    public <T extends Model> List<T> getAll(Class<T> type) {
-        return getRepository(type).getAll();
+    public <T extends Model> Collection<T> selectAll(Class<T> type) {
+        return getRepository(type).selectAll();
     }
 
-    public <T extends Model> Optional<T> getById(Class<T> type, int id) {
-        return getRepository(type).getById(id);
+    public <T extends Model> Optional<T> selectById(Class<T> type, int id) {
+        return getRepository(type).selectById(id);
     }
 
-    public <T extends Model> void create(Class<T> type, T model) {
-        getRepository(type).create(model);
+    public <T extends Model> void insert(Class<T> type, T model) {
+        getRepository(type).insert(model);
     }
-    
-    public <T extends Model> void edit(Class<T> type, int id, Consumer<T> editer) {
-        getRepository(type).edit(id, editer);
+
+    public <T extends Model> void update(Class<T> type, T model) {
+        getRepository(type).update(model);
     }
 
     public <T extends Model> void delete(Class<T> type, int id) {
         getRepository(type).delete(id);
-    }
-
-    public void save() {
-        for (Model model : Model.CreatedModels) {
-            model.State = Model.ModelState.Unchanged;
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-        Model.CreatedModels.clear();
-        
-        for (Model model : Model.EditedModels) {
-            model.State = Model.ModelState.Unchanged;
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-        Model.EditedModels.clear();
-        
-        for (Model model : Model.DeletedModels) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-        Model.DeletedModels.clear();
     }
 }
